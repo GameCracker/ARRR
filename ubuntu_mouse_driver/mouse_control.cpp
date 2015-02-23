8ce1a91330421814596f24f4ba584231b0031fd7
GameCracker <ziqikelly@gmail.com>
2015-02-22 21:17:57 -0800

    update mouse move and click

--- ubuntu_mouse_driver/mouse_control.cpp
+++ ubuntu_mouse_driver/mouse_control.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -33,11 +35,110 @@ int exec1(string& cmd) {
   return 0;
 }
 
-int main(int argc, char *argv[]) {
-  string str = "ls";
-  getline(cin, str);
-  cout << "Command entered: " << str << endl;
-  exec1(str);
+string exec2(string& cmd) {
+  FILE* in;
+  char buf[512];
+  if(!(in = popen(cmd.c_str(), "r"))) {
+    return "";
+  }
+  while(fgets(buf, sizeof(buf), in) != NULL) {
+    return string(buf);
+  }
+  pclose(in);
+  return "";
 }
+
+string& trim_left_in_place(string& str) {
+    size_t i = 0;
+    while(i < str.size() && isspace(str[i])) { 
+      ++i; 
+    };
+    return str.erase(0, i);
+}
+
+string& trim_right_in_place(string& str) {
+    size_t i = str.size();
+    while(i > 0 && isspace(str[i - 1])) { 
+      --i; 
+    };
+    return str.erase(i, str.size());
+}
+
+string& trim_in_place(string& str) {
+    return trim_left_in_place(trim_right_in_place(str));
+}
+
+// returns newly created strings
+string trim_right(string str) {
+    return trim_right_in_place(str);
+}
+
+string trim_left(string str) {
+    return trim_left_in_place(str);
+}
+
+string trim(string str) {
+    return trim_left_in_place(trim_right_in_place(str));
+}
+
+// right and left click by eye blink times
+int click(int time) {
+  int click;
+  if(time == 1) 
+    click = 1;
+  if(time == 2)
+    click = 3;
+  return click;
+}
+
+// move and click cursor according to center of pupil
+int move_cursor(int w, int h, double x, double y, int t) {
+  //  int scr_w = GetSystemMetrics(SM_CXSCREEN);
+  //int scr_h = GetSystemMetrics(SM_CYSCREEN);
+  string cmd_dim = "xdpyinfo | grep dimensions";
+  string dim = exec2(cmd_dim);
+  dim = dim.substr(dim.find(":") + 5, 8);
+  string scr_w_str = dim.substr(0, dim.find("x"));
+  string scr_h_str = dim.substr(dim.find("x") + 1);
+  scr_h_str = trim_right(scr_h_str);
+  int scr_w = atoi(scr_w_str.c_str());
+  int scr_h = atoi(scr_h_str.c_str());
+  int pos_x = (int)(x/((double)w))*((double)scr_w);
+  int pos_y = (int)(y/((double)h))*((double)scr_h);
+  string cmd = "xdotool mousemove ";
+  ostringstream ss;
+  ss << pos_x;
+  cmd.append(ss.str());
+  ss.clear();
+  ss.str("");
+  ss << pos_y;
+  cmd.append(" ");
+  cmd.append(ss.str());
+  if(t > 0) {
+    if(t > 2)
+      t = 2;
+    cmd.append(" click ");
+    ss.clear();
+    ss.str("");
+    cout << "t: " << t << endl;
+    cout << "click(t): " << click(t) << endl;
+    ss << click(t);
+    cmd.append(ss.str());
+  }
+  cout << "cursor cmd received: " << cmd << endl;
+  exec1(cmd);
+}
+
+int main(int argc, char *argv[]) {
+  string str;
+  //getline(cin, str);
+  move_cursor(300, 300, 150, 150, 1);
+  //cout << "Command entered: " << str << endl;
+  //exec1(str);
+}
+
+
+
+
 
 
