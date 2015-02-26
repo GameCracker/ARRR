48060f8f733c40a50e40bbb1a6ef1c387e7094ef
Kelly Peng <ziqikelly@gmail.com>
2015-02-25 19:04:12 -0800

    ultrasound

---
+++ snippet/ultrasound/arduino_ultrasound/uno_ultrs/uno_ultrs.ino
@@ -0,0 +1,37 @@
+#define trigPin 13
+#define echoPin 12
+#define led 11
+#define led1 10
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT); 
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+  duration = (pulseIn(echoPin, HIGH);
+  distance = (duration/2)/29.1;
+  if(distance < 4) {
+    digitalWrite(led, HIGH);
+    digitalWrite(led1, LOW);
+  }
+  else {
+    digitalWrite(led, LOW);
+    digitalWrite(led1, HIGH);  
+  }
+  if(distance >= 200 || distance <= 0) {
+    Serial.println("Out of range"); 
+  }
+  else {
+    Serial.print(distance);
+    Serial.printlm(" cm"); 
+  }
+  delay(500);
+}
